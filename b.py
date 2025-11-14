04185c441ee3c1530ad73bca99948cee86ec4a24
tp <tp@taipi.top>
2025-11-14 10:28:41 +0800

    first commit

---
+++ b.py
@@ -0,0 +1,12 @@
+import asyncio
+from fastmcp import Client
+
+async def main():
+    async with Client("https://gofastmcp.com/mcp") as client:
+        result = await client.call_tool(
+            name="SearchFastMcp",
+            arguments={"query":"deploy a FastMCP server"}
+        )
+        print(result)
+
+asyncio.run(main())
